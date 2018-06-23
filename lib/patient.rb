528ca1d90209e748e1b7951eab8bca8007861a9b
Kevin Chi <kevinchi118@gmail.com>
2018-06-23 02:40:11 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,4 +1,16 @@
 class Patient
 
     attr_accessor :name
-    
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def new_appointment(doctor, date)
+        Appointment.new()
+
+
+end
